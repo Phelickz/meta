78db03f4af03f128892be00dbc7ce20ac1d19e55
Ezeevidence <eze.pleasant234@gmail.com>
2023-08-21 23:38:05 +0100

    added notifications settings

--- lib/ui/widgets/settings/settings.dart
+++ lib/ui/widgets/settings/settings.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:meta_trader/app/utils/dimensions.dart';
 import 'package:meta_trader/ui/views/settings/settings_view_model.dart';
+import 'package:meta_trader/ui/widgets/settings/components/settings_tile.dart';
+
+import '../../../app/responsiveness/res.dart';
+import '../../../app/utils/theme.dart';
 
 class SettingsPage extends StatelessWidget {
   final SettingsViewModel model;
@@ -7,6 +12,77 @@ class SettingsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var isDarkMode = CustomThemeData.isDarkMode(context);
+
+    return SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: McGyver.rsDoubleW(context, 6),
+        ),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 15.pHeight(context),
+            ),
+            Text(
+              "General",
+              style: CustomThemeData.generateStyle(
+                fontSize: McGyver.textSize(context, 1.5),
+                color: isDarkMode ? Colors.white54 : const Color(0xff667085),
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+            SizedBox(
+              height: 15.pHeight(context),
+            ),
+            BasicSettingsTile(title: "Push Notifications"),
+            BasicSettingsTile(title: "Language"),
+            SizedBox(
+              height: 40.pHeight(context),
+            ),
+            Text(
+              "Appearance",
+              style: CustomThemeData.generateStyle(
+                fontSize: McGyver.textSize(context, 1.5),
+                color: isDarkMode ? Colors.white54 : const Color(0xff667085),
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+            SizedBox(
+              height: 200.pHeight(context),
+            ),
+            Text("Market",
+                style: CustomThemeData.generateStyle(
+                  fontSize: McGyver.textSize(context, 1.5),
+                  color: isDarkMode ? Colors.white54 : const Color(0xff667085),
+                  fontWeight: FontWeight.normal,
+                )),
+            BasicSettingsTile(title: "Change Basis"),
+            SizedBox(
+              height: 40.pHeight(context),
+            ),
+            Text("Others",
+                style: CustomThemeData.generateStyle(
+                  fontSize: McGyver.textSize(context, 1.5),
+                  color: isDarkMode ? Colors.white54 : const Color(0xff667085),
+                  fontWeight: FontWeight.normal,
+                )),
+            BasicSettingsTile(
+              title: "Privacy Policy",
+              isArrowTrailing: false,
+              secondaryTrailing: SizedBox(),
+            ),
+            BasicSettingsTile(
+              title: "About Us",
+            ),
+            BasicSettingsTile(
+              title: "Current version",
+              isArrowTrailing: false,
+              secondaryTrailing: SizedBox(),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
