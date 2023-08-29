edb3d0fbefa224e72affaca1db731ea54b3a75f2
petprog <farinupeter@gmail.com>
2023-08-29 14:17:22 +0100

    change the app config to default

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:meta_trader/app/locator/locator.dart';
 import 'package:meta_trader/app/services/router_service.dart';
 import 'package:meta_trader/app/utils/theme.dart';
-import 'package:meta_trader/ui/views/trade_history/trade_history_view.dart';
 import 'package:stacked_themes/stacked_themes.dart';
 
 class MetaTraderApp extends StatelessWidget {
@@ -18,24 +17,24 @@ class MetaTraderApp extends StatelessWidget {
       darkTheme: CustomThemeData.darkTheme,
       lightTheme: CustomThemeData.lightTheme,
       builder: (context, regularTheme, darkTheme, themeMode) {
-        // return MaterialApp.router(
-        //   title: "Meta Trader",
-        //   debugShowCheckedModeBanner: false,
-        //   routeInformationParser: _routerService.router.defaultRouteParser(),
-        //   routerDelegate: _routerService.router.delegate(),
-        //   theme: regularTheme,
-        //   themeMode: themeMode,
-        //   // theme: ThemeData(brightness: Brightness.light, fontFamily: 'Gilroy'),
-        //   darkTheme: darkTheme,
-        // );
-
-        return MaterialApp(
+        return MaterialApp.router(
+          title: "Meta Trader",
           debugShowCheckedModeBanner: false,
+          routeInformationParser: _routerService.router.defaultRouteParser(),
+          routerDelegate: _routerService.router.delegate(),
           theme: regularTheme,
           themeMode: themeMode,
+          // theme: ThemeData(brightness: Brightness.light, fontFamily: 'Gilroy'),
           darkTheme: darkTheme,
-          home: const TradeHistoryView(),
         );
+
+        //   return MaterialApp(
+        //     debugShowCheckedModeBanner: false,
+        //     theme: regularTheme,
+        //     themeMode: themeMode,
+        //     darkTheme: darkTheme,
+        //     home: const TradeHistoryView(),
+        //   );
       },
     );
   }
