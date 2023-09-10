a166f4f9431428a437c72e890da7118d407daa6e
Ezeevidence <eze.pleasant234@gmail.com>
2023-09-10 09:33:05 +0100

    provider

--- lib/ui/widgets/provider/provider_login.dart
+++ lib/ui/widgets/provider/provider_login.dart
@@ -14,43 +14,86 @@ class ProviderLoginPage extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        SizedBox(height: 24.pHeight(context),),
+        SizedBox(
+          height: 24.pHeight(context),
+        ),
         ProviderFormFields.simpleField("Account ID", "Enter your account ID"),
-        SizedBox(height: 16.pHeight(context),),
+        SizedBox(
+          height: 16.pHeight(context),
+        ),
         ProviderFormFields.simpleFieldWithPassword("Password", "Password"),
-         SizedBox(height: 16.pHeight(context),),
-        ProviderFormFields.simpleFieldWithIcon("Desired Fee", "0%", IconButton(onPressed: (){}, icon: const Icon(Icons.arrow_forward_ios_outlined))),
-        SizedBox(height: 20.pHeight(context),),
+        SizedBox(
+          height: 16.pHeight(context),
+        ),
+        ProviderFormFields.simpleFieldWithIcon(
+            "Desired Fee",
+            "0%",
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.arrow_forward_ios_outlined))),
+        SizedBox(
+          height: 20.pHeight(context),
+        ),
         Row(
           children: [
-            Checkbox(value: viewModel.checkbox, onChanged: (_) => viewModel.toggleCheckbox()),
-            SizedBox(width: 12.pWidth(context),),
+            Checkbox(
+                value: viewModel.checkbox,
+                onChanged: (_) => viewModel.toggleCheckbox()),
+            SizedBox(
+              width: 12.pWidth(context),
+            ),
             Expanded(
-              child: RichText(
-                text: TextSpan(
+                child: RichText(
+              text: TextSpan(
                   text: "I agree with the",
-                  style: CustomThemeData.generateColoredStyle(fontSize: 12, context: context,fontWeight: FontWeight.w400,lightTextColor: const Color(0xFF98A2B3), darkTextColor: const Color(0xFF98A2B3)),
+                  style: CustomThemeData.generateColoredStyle(
+                      fontSize: 12,
+                      context: context,
+                      fontWeight: FontWeight.w400,
+                      lightTextColor: const Color(0xFF98A2B3),
+                      darkTextColor: const Color(0xFF98A2B3)),
                   children: [
                     TextSpan(
                       text: "terms and conditions",
-                      style: CustomThemeData.generateColoredStyle(fontSize: 12, context: context,fontWeight: FontWeight.w400,lightTextColor: const Color(0xFF47B0F5), darkTextColor: const Color(0xFF47B0F5)),
+                      style: CustomThemeData.generateColoredStyle(
+                          fontSize: 12,
+                          context: context,
+                          fontWeight: FontWeight.w400,
+                          lightTextColor: const Color(0xFF47B0F5),
+                          darkTextColor: const Color(0xFF47B0F5)),
                     ),
                     TextSpan(
                       text: "for opening an account and the",
-                      style: CustomThemeData.generateColoredStyle(fontSize: 12, context: context,fontWeight: FontWeight.w400,lightTextColor: const Color(0xFF98A2B3), darkTextColor: const Color(0xFF98A2B3)),
+                      style: CustomThemeData.generateColoredStyle(
+                          fontSize: 12,
+                          context: context,
+                          fontWeight: FontWeight.w400,
+                          lightTextColor: const Color(0xFF98A2B3),
+                          darkTextColor: const Color(0xFF98A2B3)),
                     ),
                     TextSpan(
                       text: "data protection policy",
-                      style: CustomThemeData.generateColoredStyle(fontSize: 12, context: context,fontWeight: FontWeight.w400,lightTextColor: const Color(0xFF47B0F5), darkTextColor: const Color(0xFF47B0F5)),
+                      style: CustomThemeData.generateColoredStyle(
+                          fontSize: 12,
+                          context: context,
+                          fontWeight: FontWeight.w400,
+                          lightTextColor: const Color(0xFF47B0F5),
+                          darkTextColor: const Color(0xFF47B0F5)),
                     )
-                  ]
-                ),
-              )
-            )
+                  ]),
+            ))
           ],
         ),
         Spacer(),
-        CustomButtons.generalButton(context: context, onTap: (){}, text: "Register",width: double.infinity, textSize: 1.8,),
+        CustomButtons.generalButton(
+          context: context,
+          onTap: () {
+            viewModel.setProviderPageEnum = ProviderPageEnum.dashboard;
+          },
+          text: "Register",
+          width: double.infinity,
+          textSize: 1.8,
+        ),
       ],
     );
   }
